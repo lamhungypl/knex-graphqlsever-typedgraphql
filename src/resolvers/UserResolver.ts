4639b7435790a258e589cf502e71ba661d06cb2f
lamhung <lamhungypl@gmail.com>
2021-06-30 16:36:24 +0700

    user login

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,11 +1,23 @@
 import { ApolloError } from 'apollo-server';
 import { Knex } from 'knex';
-import { Query, Resolver, Ctx, Arg, Mutation } from 'type-graphql';
+import { Query, Resolver, Ctx, Arg, Mutation, Field, ObjectType } from 'type-graphql';
 import { RegisterInput } from '../dto/RegisterInput';
 import { UpdateInput } from '../dto/UpdateInput';
 import { User } from '../entities/User';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 import { ChangePasswordInput } from '../dto/ChangePassword';
+import { LoginInput } from '../dto/LoginInput';
+
+@ObjectType()
+class AuthResponse {
+  @Field()
+  token: string;
+
+  @Field()
+  user: User;
+}
+
 @Resolver()
 export class UserResolver {
   // constructor(private bookService: B∂ookService) {}
@@ -49,6 +61,30 @@ export class UserResolver {
       throw new ApolloError(error.message);
     }
   }
+
+  @Mutation(() => AuthResponse)
+  async login(@Arg('input') input: LoginInput, @Ctx() ctx) {
+    const db: Knex = ctx.db;
+    const { user_id, password } = input;
+    try {
+      const [user] = await db('users').where({ user_id });
+      if (!user) {
+        throw new ApolloError('User not found');
+      }
+      const matchPass = await bcrypt.compare(password, user.password);
+      if (!matchPass) {
+        throw new ApolloError('Username/ password wrong');
+      }
+
+      const token = jwt.sign({ id: user.user_id }, 'AAA123', {
+        expiresIn: 3600,
+      });
+      return { token, user };
+    } catch (error) {
+      throw new ApolloError(error.message);
+    }
+  }
+
   @Mutation(() => User)
   async addUser(@Arg('input') input: RegisterInput, @Ctx() ctx) {
     const db: Knex = ctx.db;
